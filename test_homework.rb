f5864e2528ccd4d176c2967f3d9f2c393344f76f
Sam Wheeler <swheeler111@gmail.com>
2016-04-15 11:46:44 -0400

    completed first two steps

--- test_homework.rb
+++ test_homework.rb
@@ -1,7 +1,8 @@
 # write a line of code that multiplies 3 * 5 and prints the result to the terminal
-
+puts (3 *5)
 
 # Use a string method to print out the length of your name
-
+my_name = "sam"
+puts sam.length
 
 # Yet another update
